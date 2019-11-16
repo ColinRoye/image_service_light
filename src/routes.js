0b1e827d39887f4a1536b1475f5f461037f6bd39
ColinRoye <croye97@gmail.com>
2019-11-16 13:56:33 -0500

    edit to add media

--- src/routes.js
+++ src/routes.js
@@ -15,14 +15,15 @@ router.post('/addmedia', upload.single('content'), async (req, res, next)=>{
     let id = shortid.generate();
     debug.log(req.cookies["auth"])
     ret = await services.deposit(id, req.file,res, req.cookies["auth"]);
-    if(ret.status == env.statusError.status){
-      ret.error = "anything"
-    }
+    // if(ret.status == env.statusError.status){
+    //   ret.error = "anything"
+    // }
   }else{
      ret = env.statusError;
      ret.error = "not authorized";
+     res.send(ret);
+
   }
-  res.send(ret);
 
 
 });
@@ -36,15 +37,11 @@ router.delete('/media/:id', async (req, res, next)=>{
      console.log(filename);
      let ret = services.delete(filename,res);
 });
-router.get('/used/:id', async (req, res, next)=>{
-  if(req.cookies["auth"]){
+router.get('/used/:id/:username', async (req, res, next)=>{
+      let filename = req.params.id
+      console.log(filename);
+      let ret = services.getUsed(filename,res, req.params.username);
 
-     let filename = req.params.id
-     console.log(filename);
-     let ret = services.getUsed(filename,res, req.cookies["auth"]);
-   }else{
-     res.send({used: '-3'});
-   }
 });
 router.post('/used/:id', async (req, res, next)=>{
      let filename = req.params.id
